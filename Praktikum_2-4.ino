e25ab4e5f0febd384b39fed474c205a0bcc8952d
azizmaulana <vvvvvv.ateng@gmail.com>
2016-08-30 19:48:12 +0700

    Add files via upload

---
+++ Praktikum_2-4.ino
@@ -0,0 +1,17 @@
+void setup() {
+pinMode(A0, INPUT);
+digitalWrite(A0, 1);
+pinMode(12, OUTPUT);
+
+}
+
+void loop() {
+if(digitalRead(A0)==0){
+  digitalWrite(12, 1);
+  
+}
+else {
+  digitalWrite(12,0);
+}
+}
+
